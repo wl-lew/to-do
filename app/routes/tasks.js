579f4b204fcc8ec2b229f4c20e248d8c7802e2d2
WL <wladek284w@gmail.com>
2025-01-05 10:08:42 +0100

    FEAT: create find task by ID endpoint

--- app/routes/tasks.js
+++ app/routes/tasks.js
@@ -22,21 +22,37 @@ task.post('/create', async (req, res) => {
     }
     catch (err) {
         res.status(500).json({
-            message: "Error",
-            error: err.message
+            message: "Error"
         });
     }
 });
 
-task.get('/show', async (req, res) => {
+task.get('/show-all', async (req, res) => {
     try {
         const tasks = await Task.find();
         res.status(200).json(tasks);
     }
     catch (err) {
         res.status(500).json({
-            message: "Error",
-            error: err.message
+            message: "Error"
+        });
+    }
+});
+
+task.get('/show/:id', async (req, res) => {
+    try {
+        const task = await Task.findById(req.params.id);
+
+        if (!task) {
+            return res.status(404).json({
+                message: "Task not found!"
+            });
+        }
+        res.status(200).json(task);
+    }
+    catch (err) {
+        res.status(500).json({
+            message: "Error retrieving!"
         });
     }
 });
