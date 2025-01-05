31a2fafc27c66c3b2ae6500f0450a8e9fb6f89d8
WL <wladek284w@gmail.com>
2025-01-05 09:32:22 +0100

    FEAT: create task GET endpoint

--- app/routes/tasks.js
+++ app/routes/tasks.js
@@ -3,7 +3,7 @@ import Task from "../../db/models/taskModel.js";
 
 const task = express.Router();
 
-task.post('/tasks', async (req, res) => {
+task.post('/create', async (req, res) => {
     try {
         const { title, description, status } = req.body;
 
@@ -22,7 +22,21 @@ task.post('/tasks', async (req, res) => {
     }
     catch (err) {
         res.status(500).json({
-            message: "Error", err
+            message: "Error",
+            error: err.message
+        });
+    }
+});
+
+task.get('/show', async (req, res) => {
+    try {
+        const tasks = await Task.find();
+        res.status(200).json(tasks);
+    }
+    catch (err) {
+        res.status(500).json({
+            message: "Error",
+            error: err.message
         });
     }
 });
