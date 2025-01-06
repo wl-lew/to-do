9f739428fe64ad64cdc2ee1b3854423870c185b7
WL <wladek284w@gmail.com>
2025-01-06 11:26:38 +0100

    FEAT: bug fixes [0001]

--- app/helpers/tasksValidation.js
+++ app/helpers/tasksValidation.js
@@ -12,7 +12,7 @@ export const validateRequireFields = (req, res) => {
 };
 
 export const paginationValidator = (req, res) => {
-    const { page = 1, limit = 10 } = req.query;
+    const { page = 1, limit = 10 } = req.query || {};
 
     if (isNaN(page) || isNaN(limit)) {
         return res.status(400).json({
@@ -25,4 +25,3 @@ export const paginationValidator = (req, res) => {
         limit: Number(limit)
     };
 };
-
